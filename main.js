c96068676c1b55d85f713293e2f5698c6b43f573
Mushfiqur06 <mushfiqur06@gmail.com>
2019-07-14 00:28:45 +0600

    Adding a personalize welcome message

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //Adding an image changer
+/*
 var myImage = document.querySelector('img');
 
 myImage.onclick = function(){
@@ -8,4 +9,26 @@ myImage.onclick = function(){
 	}else{
 		myImage.setAttribute('src', 'img_2019.jpg');
 	}
+}
+*/
+
+//Adding a personalize welcome message
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName(){
+	var myName = prompt('Please enter you name: ');
+	console.log(myName)
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Mozila Is Cool ' + myName;
+}
+if(!localStorage.getItem('name')){
+	setUserName()
+}else{
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Mozila is cool ' + storedName;
+}
+
+myButton.onclick = function(){
+	setUserName();
 }
