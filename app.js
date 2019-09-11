a0656c4649fb75d4107c27c1652bc32d13a2e49c
mushfiqur06 <mushfiqur06@gmail.com>
2019-09-11 22:29:43 +0600

    Create a sorting function

--- app.js
+++ app.js
@@ -6,3 +6,33 @@ let newString = myText.replace('String', 'I am another String');
 let myArray = ['I', 'love', 'JavaScript'];
 let madeString = myArray.join(' ');
 console.log(madeString);
+
+//learn array sorting
+let arr = [12, 45, 67, 90, 1, 46, 678];
+
+//Create a sorting function
+function sort(arr, cb){
+    for(var i = 0; i < arr.length; i++){
+        for(var j = 0; j < i; j++){
+            var result = cb(arr[i], arr[j]);
+
+            if(result < 0){
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+sort(arr, (a, b) => {
+    if(a > b){
+        return 1;
+    }else if(a < b){
+        return -1;
+    }else{
+        return 0;
+    }
+});
+
+console.log(arr)
