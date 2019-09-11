72c45c1b796500474b99e8537e66f954e04c647f
mushfiqur06 <mushfiqur06@gmail.com>
2019-09-11 23:19:05 +0600

    Learn sorting

--- app.js
+++ app.js
@@ -8,31 +8,58 @@ let madeString = myArray.join(' ');
 console.log(madeString);
 
 //learn array sorting
-let arr = [12, 45, 67, 90, 1, 46, 678];
+// let arr = [12, 45, 67, 90, 1, 46, 678];
 
 //Create a sorting function
-function sort(arr, cb){
-    for(var i = 0; i < arr.length; i++){
-        for(var j = 0; j < i; j++){
-            var result = cb(arr[i], arr[j]);
+// function sort(arr, cb){
+//     for(var i = 0; i < arr.length; i++){
+//         for(var j = 0; j < i; j++){
+//             var result = cb(arr[i], arr[j]);
 
-            if(result < 0){
-                var temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
+//             if(result < 0){
+//                 var temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//     }
+// }
+
+// sort(arr, (a, b) => {
+//     if(a > b){
+//         return 1;
+//     }else if(a < b){
+//         return -1;
+//     }else{
+//         return 0;
+//     }
+// });
+
+// console.log(arr)
+
+let homes = [
+    {
+        "h_id": "3",
+        "city": "Dallas",
+        "state": "TX",
+        "zip": "95201",
+        "price": "162500"
+    }, {
+        "h_id": "4",
+        "city": "Bevery Hills",
+        "state": "CA",
+        "zip": "75201",
+        "price": "200"
+    }, {
+        "h_id": "5",
+        "city": "New York",
+        "state": "NY",
+        "zip": "00010",
+        "price": "962500"
     }
-}
+];
 
-sort(arr, (a, b) => {
-    if(a > b){
-        return 1;
-    }else if(a < b){
-        return -1;
-    }else{
-        return 0;
-    }
-});
-
-console.log(arr)
+homes.sort((a, b) => {
+    return parseFloat(a.price) - parseFloat(b.price);
+})
+console.log(homes)
