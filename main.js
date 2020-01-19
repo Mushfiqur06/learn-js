5c8b1f8f646f411401a11587517ca3d88745e9ba
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-01-19 13:18:03 +0600

    get large number of array

--- main.js
+++ main.js
@@ -521,3 +521,21 @@ console.log(book2.wishList());
 
 // const result = new Person('Mushfiqur Niloy', 23);
 // console.log(result.print())
+
+function largestOfFour(arr) {
+	let longestArray = [];
+
+	for(let i = 0; i < arr.length; i++){
+		const newArray = arr[i].sort((a, b) => b - a)
+		longestArray.push(newArray.shift())
+	}
+
+	return longestArray;
+
+  }
+  
+  largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+const arrays = [4, 5, 1, 3].sort((a, b) => b - a);
+
+console.log(arrays.shift())
