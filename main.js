582c09bebaff959d994b263d9c3db6587cc85eb7
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-01-22 15:20:22 +0600

    Larn DRY

--- main.js
+++ main.js
@@ -624,4 +624,27 @@ let dropdownData = [
 ]
 
 
-console.log(dropdownData.map(item => item.name))
+function Cat(name) {
+	this.name = name;
+  }
+  
+  Cat.prototype = {
+	constructor: Cat
+  };
+  
+  function Bear(name) {
+	this.name = name;
+  }
+  
+  Bear.prototype = {
+	constructor: Bear
+  };
+  
+  function Animal() { }
+  
+  Animal.prototype = {
+	constructor: Animal,
+  eat: function() {
+	  console.log("nom nom nom");
+	}
+  };
