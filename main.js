0d6a92b2b3f8617126041b274c7be7a85d0cd087
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-02 22:56:40 +0600

    Learn IIFE

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-function Bird(){
-    let weight = 15
-
-    this.getWeight = function(){
-        return weight
-    }
-}
-
-let result = new Bird()
-console.log(result.weight)
+(function(){
+    console.log('Hello')
+})()
