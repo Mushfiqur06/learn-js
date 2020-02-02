90545df45675dcdb932a715df1f9fbd78df73557
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-02 23:16:16 +0600

    learn IIFE

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-(function(){
-    console.log('Hello')
+let duck = {
+    name: 'Mushfiqur'
+}
+let motionModule = (function(){
+    return {
+        glideMixin: function(obj){
+            obj.glide = function(){
+                console.log("Gliding on the water")
+            }
+        },
+        flyMixin: function(obj){
+            obj.fly = function(){
+                console.log("Flying, wooosh!")
+            }
+        }
+    }
 })()
+
+motionModule.flyMixin(duck)
+
+console.log(duck.fly())
