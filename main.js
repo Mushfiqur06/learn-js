215932fbe32f7b7ffbf7d9d5b9e82dd693dce92d
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-05 23:13:13 +0600

    Learning Mution

--- main.js
+++ main.js
@@ -1,28 +1,10 @@
-var Window = function(tabs){
-    this.tabs = tabs
+var fixed = 4;
+
+function increment(){
+    var result = fixed + 1;
+    return result
 }
 
-Window.prototype.join = function(otherWindow){
-    this.tabs = this.tabs.concat(otherWindow.tabs)
-
-    return this
-}
-
-Window.prototype.tabOpen = function(tab){
-    this.tabs.push('New Tab')
-
-    return this
-}
-
-Window.prototype.tabClose = function(index){
-    var tabBeforeIndex = this.tabs.splice(0, index)
-    var tabAfterIndex = this.tabs.splice(1)
-
-    this.tabs = tabBeforeIndex.concat(tabAfterIndex)
-
-    return this
-}
-
-var workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']);
-var finalTabs = workWindow.tabOpen()
-console.log(finalTabs)
+var another = increment()
+console.log(another)
+console.log(fixed)
