aeeb84bec78d779e5b6a25c15e61386376f92757
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-05 20:08:21 +0600

    Reading Variable from YDKJS

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-var amount  = 42;
-amount = amount + 2;
-amount = '$' + String(amount)
+var TEXT_RATE = 0.88
+var amount = 99.99
+amount = amount * 2
+amount = amount + (amount * TEXT_RATE)
 
 console.log(amount)
+console.log(amount.toFixed(2))
