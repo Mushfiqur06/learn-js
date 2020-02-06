a8bb7ec5d705dee0757312a9b40e4959ff50e4c8
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-06 08:38:30 +0600

    Reading Book

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function printAmount(){
-    console.log(amount.toFixed(2))
+function printAmount(amt){
+    console.log(amt.toFixed(2))
+}
+
+function formatAmount(){
+    return '$' + amount.toFixed(2)
 }
 
 var amount = 99.99
-console.log(printAmount())
+
+printAmount(amount * 2)
+amount = formatAmount()
+console.log(amount)
