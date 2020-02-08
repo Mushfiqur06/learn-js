54d2eaf732a2d81f2a3153350b21a07b7abf1be0
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-08 23:43:18 +0600

    dkndjnvf

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
 
 function add(bookName) {
-    bookList.push(bookName);
-    return bookList;
-
-    // Add your code above this line
+    return [...bookList, bookName];
 }
 
 var newBookList = add(bookList, 'A Brief History of Time');
+console.log(newBookList)
