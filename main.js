aa850e405ac7a8d303748bbb1ae6b917b6db0a1a
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-10 00:40:40 +0600

    Problem solving

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-function outer(){
-    var a = 2;
+let bank_balance = 303.91;
+const SPENDING_THRESSHOLD = 200;
+const PHONE_PRICE = 99.00;
+const ACCESSORY_PRICE = 9.00
+let amount = 0;
+const TEX_RATE = 0.80;
 
-    function inner(){
-        var b = 3;
-        console.log(a + b)
-    }
-
-    inner()
+function calculateTexRate(amount){
+    return amount * TEX_RATE;
 }
 
-outer()
+
+while(amount < bank_balance){
+    amount = amount + PHONE_PRICE;
+    console.log(amount)
+
+    if(amount < SPENDING_THRESSHOLD){
+        amount  = amount + ACCESSORY_PRICE;
+        console.log(amount)
+    }
+}
+
+if(amount > bank_balance){
+    console.log('You can not affort this purchase')
+}
