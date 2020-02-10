6adc76c86cb510e61a21c4402fff0a1d4318f311
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-11 00:06:15 +0600

    Reading Book

--- main.js
+++ main.js
@@ -1,25 +1,9 @@
-let bank_balance = 303.91;
-const SPENDING_THRESSHOLD = 200;
-const PHONE_PRICE = 99.00;
-const ACCESSORY_PRICE = 9.00
-let amount = 0;
-const TEX_RATE = 0.80;
-
-function calculateTexRate(amount){
-    return amount * TEX_RATE;
+function foo(){
+    return "Something"
 }
 
+foo.bar = 21;
 
-while(amount < bank_balance){
-    amount = amount + PHONE_PRICE;
-    console.log(amount)
-
-    if(amount < SPENDING_THRESSHOLD){
-        amount  = amount + ACCESSORY_PRICE;
-        console.log(amount)
-    }
-}
-
-if(amount > bank_balance){
-    console.log('You can not affort this purchase')
-}
+console.log(typeof foo)
+console.log(typeof foo())
+console.log(typeof foo.bar)
