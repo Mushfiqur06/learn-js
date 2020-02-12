60ed29eccc222977e867eac8e50721647da8cc59
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-12 08:35:26 +0600

    Reading Book

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-function foo(){
-    var a = 1;
-
-    function bar(){
-        var b = 2;
-
-        function baz(){
-            var c = 3;
-
-            console.log(a, b, c)
-        }
-
-        baz()
-    }
-    bar()
-
-}
-foo()
+(function hello(){
+    console.log('Hello')
+})()
