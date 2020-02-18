872f676eab9bf15a2cdedf32ece460aa7948c621
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-02-18 21:35:30 +0600

    Learn This

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-(function hello(){
-    console.log('Hello')
-})()
+function foo(){
+    console.log(this.bar);
+}
+
+var bar = 'global'
+
+var obj1 = {
+    bar: 'obj1',
+    foo: foo
+}
+var obj2 = {
+    bar: 'obj2'
+}
+
+console.log(foo())
+console.log(obj1.foo())
+console.log(foo.call(obj2))
