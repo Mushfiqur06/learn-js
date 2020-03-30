aca2f6e168d435117b4faf27a629d3293cc91b78
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-03-30 10:45:44 +0600

    Learning JS Principle

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-var foo = {
-    a: 42
+// var foo = {
+//     a: 42
+// }
+// var bar = Object.create(foo)
+// bar.b = 'Hello'
+// console.log(bar.a)
+// console.log(bar.b)
+
+const num = 3;
+
+function multipleBy2(inputNum) {
+  const result = inputNum * 2;
+  return result;
 }
-var bar = Object.create(foo)
-bar.b = 'Hello'
-console.log(bar.a)
-console.log(bar.b)
+
+const output = multipleBy2(num);
+const newOutput = multipleBy2(10);
+
+console.log(output);
+console.log(newOutput);
