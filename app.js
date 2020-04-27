ecfe5e13e549b2356250b0e7b98fc738c5eccca0
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-04-27 23:53:12 +0600

    Factorial Function Problem Solve

--- app.js
+++ app.js
@@ -222,15 +222,14 @@
 // fun(a);
 // console.log(a.some);
 
-const add = (function () {
-	let counter = 0;
+function factorial(n) {
+	if (n === 0 || n === 1) {
+		return 1;
+	}
+	for (var i = n - 1; i >= 1; i--) {
+		n *= i;
+	}
+	return n;
+}
 
-	return () => {
-		console.log('Hello' + counter);
-		return (counter += 1);
-	};
-})();
-
-console.log(add());
-console.log(add());
-console.log(add());
+console.log(factorial(4));
