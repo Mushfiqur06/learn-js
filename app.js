2dd3f502bac7d4bcfe3f4f22f36e3067450516be
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-05 17:57:28 +0600

    fkdkfdnf

--- app.js
+++ app.js
@@ -411,9 +411,9 @@
 // userCreator.prototype.increment = function () {
 //   console.log(this.score++);
 // };
-userCreator.prototype.login = function () {
-  console.log("Login");
-};
+// userCreator.prototype.login = function () {
+//   console.log("Login");
+// };
 const user1 = new userCreator("Niloy", 9);
 user1.increment();
 user1.increment();
