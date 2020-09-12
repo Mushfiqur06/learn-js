6f78ae0e236384cdd842f6b1ae963db8f201fc35
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-12 20:51:21 +0600

    ddfnnd

---
+++ helper.js
@@ -0,0 +1,28 @@
+export function getID(id) {
+  try {
+    return document.getElementById(id);
+  } catch (err) {
+    console.log(err);
+  }
+}
+export function query(term) {
+  try {
+    return document.getElementById(term);
+  } catch (err) {
+    console.log(err);
+  }
+}
+export function queryAll(term) {
+  try {
+    return document.getElementById(term);
+  } catch (err) {
+    console.log(err);
+  }
+}
+export function getClass(term) {
+  try {
+    return document.getElementById(term);
+  } catch (err) {
+    console.log(err);
+  }
+}
