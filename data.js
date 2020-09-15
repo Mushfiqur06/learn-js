6bf0a33382d745de65bd2d6e41262e4472ff8e16
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-15 20:17:23 +0600

    Add data

---
+++ data.js
@@ -0,0 +1,34 @@
+const book = [
+  {
+    name: "Book Name One",
+    price: 34,
+    id: 1,
+  },
+  {
+    name: "Book Name Two",
+    price: 34,
+    id: 2,
+  },
+  {
+    name: "Book Name Three",
+    price: 34,
+    id: 3,
+  },
+  {
+    name: "Book Name Four",
+    price: 34,
+    id: 4,
+  },
+  {
+    name: "Book Name Five",
+    price: 34,
+    id: 5,
+  },
+  {
+    name: "Book Name Six",
+    price: 34,
+    id: 6,
+  },
+];
+
+module.exports = book;
