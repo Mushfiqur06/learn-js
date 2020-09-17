90b14d93c4bc0d8d3b6bdb9d727bd25cd8f57ffa
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-17 23:35:25 +0600

    kfdkf

--- src/app.js
+++ src/app.js
@@ -1,42 +1,82 @@
 import { getID, query, queryAll, getClass, create } from "./helper";
-import book from "./../data";
-import { generateLi } from "./bookLi";
-// let interValId;
-// let heading = getID("heading");
-// let headingArr = [heading.innerText, "Hading Change Info"];
-// let terms = false;
-// let container = getClass(".container");
-
-// setInterval(() => {
-//   if (terms) {
-//     heading.innerText = headingArr[0];
-//     terms = false;
-//     container.style.backgroundColor = "palegreen";
-//   } else {
-//     heading.innerText = headingArr[1];
-//     terms = true;
-//     container.style.backgroundColor = "palevioletred";
-//   }
-// }, 2000);
-
-// const login = getID("login");
-// const signup = getID("signup");
-// function handler(e) {
-//   alert(e.target.innerHTML);
-// }
-// login.addEventListener("click", handler);
-// signup.addEventListener("click", handler);
-const root = getID("root");
-window.onload = function () {
-  let ul = create("ul");
-  ul.onclick = eventHandler;
-  let lis = book.map((item) => ul.append(generateLi(item)));
-  ul.append(lis);
-  root.append(ul);
+let circleTop = 0;
+let circleLeft = 0;
+const containerStyle = {
+  width: "200px",
+  height: "200px",
+  margin: "0 auto",
+  border: "2px solid palegreen",
+  borderRadius: "3px",
+  position: "relative",
 };
+const circleStyle = {
+  width: "30px",
+  height: "30px",
+  borderRadius: "15px",
+  backgroundColor: "palevioletred",
+  position: "absolute",
+};
+const root = getID("root");
+const container = create("div", { class: "container" });
+Object.assign(container.style, containerStyle);
+root.append(container);
 
-function eventHandler(event) {
-  if (event.target.parentNode.tagName === "LI") {
-    alert(event.target.id);
-  }
-}
+const circle = create("div", { class: "circle" });
+container.append(circle);
+Object.assign(circle.style, circleStyle);
+const btns = create("div", { class: "btns" });
+root.append(btns);
+
+const left = create("button", { class: "left btn" });
+left.innerText = "Left";
+const right = create("button", { class: "right btn" });
+right.innerText = "Right";
+const bottom = create("button", { class: "bottom btn" });
+bottom.innerText = "Bottom";
+const top = create("button", { class: "top btn" });
+top.innerText = "Top";
+btns.append(left, right, bottom, top);
+
+const btnList = queryAll("button");
+const btnArr = Array.prototype.slice.call(btnList);
+btnArr.map((item) => {
+  console.log(item);
+  item.addEventListener("click", function (event) {
+    let circleName = event.target.innerText;
+    switch (circleName) {
+      case "Left":
+        circleLeft = circleLeft - 10;
+        circleLeft >= 0
+          ? Object.assign(circle.style, { left: `${circleLeft}px` })
+          : alert("No");
+        break;
+      case "Right":
+        circleLeft = circleLeft + 10;
+        circleLeft <= 170
+          ? Object.assign(circle.style, { left: `${circleLeft}px` })
+          : alert("No");
+        break;
+      case "Top":
+        circleTop = circleTop - 10;
+        circleTop >= 0
+          ? Object.assign(circle.style, { top: `${circleTop}px` })
+          : alert("No");
+        break;
+      case "Bottom":
+        circleTop = circleTop + 10;
+        circleTop <= 170
+          ? Object.assign(circle.style, { top: `${circleTop}px` })
+          : alert("No");
+        break;
+      default:
+        console.log("default");
+    }
+  });
+});
+
+// right.addEventListener("click", function (event) {
+//   circleLeft = circleLeft + 10;
+//   circleLeft <= 170
+//     ? Object.assign(circle.style, { left: `${circleLeft}px` })
+//     : alert("No");
+// });
