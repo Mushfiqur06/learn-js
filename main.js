87647941c731fbdf13acf98193ae1ac554e22068
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-25 23:56:06 +0600

    Match Ending String Patterns

--- main.js
+++ main.js
@@ -19,6 +19,6 @@
 // let result = calRegex.test(rickyAndCal);
 // console.log(result);
 
-let caboose = "The last car on a train is the caboose";
-let lastRegex = /caboose$/; // Change this line
-let result = lastRegex.test(caboose);
+// let caboose = "The last car on a train is the caboose";
+// let lastRegex = /caboose$/; // Change this line
+// let result = lastRegex.test(caboose);
