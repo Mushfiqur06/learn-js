55c83a57b3cff388be27ebacd7d8d0c257cfe05d
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-25 23:39:25 +0600

    Match Characters that Occur Zero or More Times

---
+++ main.js
@@ -0,0 +1,8 @@
+let score = "goooooooal";
+let gPharse = "gut feeling";
+let oPharse = "over the moon";
+let goRegex = /go*/;
+
+console.log(score.match(goRegex));
+console.log(gPharse.match(goRegex));
+console.log(oPharse.match(goRegex));
