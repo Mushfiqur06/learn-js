9963cbc706b48063521dfe1840a461c3815cb0e5
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-27 03:18:17 +0600

    Comment old code

--- main.js
+++ main.js
@@ -168,19 +168,19 @@
 // console.log(JSON.stringify(ratings));
 
 // The global variable
-var s = [23, 65, 98, 5];
+// var s = [23, 65, 98, 5];
 
-Array.prototype.myMap = function (callback) {
-  var newArray = [];
-  // Only change code below this line
-  for (var i = 0; i < s.length; i++) {
-    newArray.push(callback(s[i]));
-  }
-  // Only change code above this line
-  return newArray;
-};
+// Array.prototype.myMap = function (callback) {
+//   var newArray = [];
+//   // Only change code below this line
+//   for (var i = 0; i < s.length; i++) {
+//     newArray.push(callback(s[i]));
+//   }
+//   // Only change code above this line
+//   return newArray;
+// };
 
-var new_s = s.myMap(function (item) {
-  return item * 2;
-});
-console.log(new_s);
+// var new_s = s.myMap(function (item) {
+//   return item * 2;
+// });
+// console.log(new_s);
