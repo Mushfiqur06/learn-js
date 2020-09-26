ce7d95a86d1d3c61722790b2f0d8dc7a5d1b84d6
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-26 21:00:57 +0600

    Add view btn

--- src/app.js
+++ src/app.js
@@ -1,8 +1,39 @@
 import { getID } from "./helper";
+import { renderContactList } from "./views/viewData";
 import Form from "./views/contactForm/contactForm";
 
-const root = getID("root");
+let mode = "list";
 
-let forms = Form();
+let newBtn = getID("newContact");
+let output = getID("output");
 
-root.append(forms);
+window.onload = function () {
+  render();
+
+  newBtn.addEventListener("click", function () {
+    if (mode === "list") {
+      mode = "form";
+      newBtn.innerHTML = "Show All Contact";
+      render();
+    } else if (mode === "form") {
+      mode = "list";
+      newBtn.innerHTML = "Create New Contact";
+      render();
+    }
+  });
+};
+
+function render() {
+  if (mode === "list") {
+    output.innerHTML = "";
+    output.appendChild(renderContactList());
+  } else if (mode === "form") {
+    output.innerHTML = "";
+    let form = Form({
+      button: {
+        innerText: "submit",
+      },
+    });
+    output.appendChild(form);
+  }
+}
