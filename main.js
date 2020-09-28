1c81d5271f56d49ed8398930be13f660d3f65de7
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-28 07:22:40 +0600

    Comment old code

--- main.js
+++ main.js
@@ -341,12 +341,12 @@
 // });
 // console.log(new_s);
 
-function sliceArray(anim, beginSlice, endSlice) {
-  // Only change code below this line
-  let newArr = anim.slice(beginSlice, endSlice);
-  return newArr;
+// function sliceArray(anim, beginSlice, endSlice) {
+//   // Only change code below this line
+//   let newArr = anim.slice(beginSlice, endSlice);
+//   return newArr;
 
-  // Only change code above this line
-}
-var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
-console.log(sliceArray(inputAnim, 1, 3));
+//   // Only change code above this line
+// }
+// var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+// console.log(sliceArray(inputAnim, 1, 3));
