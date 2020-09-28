bbc99e56d2642bbc4169af686e8b1ae68c2fe5ac
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-28 07:13:35 +0600

    Implement the filter Method on a Prototype

--- main.js
+++ main.js
@@ -320,3 +320,23 @@
 // Only change code above this line
 
 // console.log(filteredList);
+
+// The global variable
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function (callback) {
+  // Only change code below this line
+  var newArray = [];
+  for (var i = 0; i < this.length; i++) {
+    if (callback(this[i]) === true) {
+      newArray.push(this[i]);
+    }
+  }
+  // Only change code above this line
+  return newArray;
+};
+
+var new_s = s.myFilter(function (item) {
+  return item % 2 === 1;
+});
+console.log(new_s);
