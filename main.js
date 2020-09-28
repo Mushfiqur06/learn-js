53fa2e091f1b770371fa59108d9a103ad114ca44
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-28 07:18:10 +0600

    Return Part of an Array Using the slice Method

--- main.js
+++ main.js
@@ -340,3 +340,13 @@
 //   return item % 2 === 1;
 // });
 // console.log(new_s);
+
+function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
+  let newArr = anim.slice(beginSlice, endSlice);
+  return newArr;
+
+  // Only change code above this line
+}
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+console.log(sliceArray(inputAnim, 1, 3));
