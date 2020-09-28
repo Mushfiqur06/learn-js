c4b30950917f1ac45efc08aba2d6248a8f8357f6
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-28 08:07:58 +0600

    Combine Two Arrays Using the concat Method

--- main.js
+++ main.js
@@ -360,3 +360,14 @@
 // }
 // var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
 // console.log(nonMutatingSplice(inputCities));
+
+function nonMutatingConcat(original, attach) {
+  // Only change code below this line
+  let newArr = original.concat(attach);
+  return newArr;
+
+  // Only change code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+console.log(nonMutatingConcat(first, second));
