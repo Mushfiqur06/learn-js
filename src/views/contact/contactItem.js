2387b031971ac448d26f324c270609462aa6526a
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-10-01 23:28:30 +0600

    dkfdf'

--- src/views/contact/contactItem.js
+++ src/views/contact/contactItem.js
@@ -1,22 +1,40 @@
 import { create } from "../../helper";
+import {
+  getContact,
+  deleteContact,
+  updateContact,
+} from "./../../contact/Contacts";
 
 export default function contactItem(props) {
   let li = create("li", { class: "listItem" });
   let avatar = create("div", { class: "avatar" });
-  avatar.innerText = props.avatar;
+  avatar.innerText = props.contact.name.charAt(0).toUpperCase();
   let divGroup = create("div");
   let p1 = create("p");
-  p1.innerText = props.name;
+  p1.innerText = props.contact.name;
   let p2 = create("p");
-  p2.innerText = props.phone;
+  p2.innerText = props.contact.phone;
   divGroup.append(p1, p2);
-  li.append(avatar, divGroup);
+  let controller = create("div");
+  let deleteBtn = create("button", { class: "btn" });
+  deleteBtn.innerText = "Delete";
 
-  li.addEventListener("click", () => props.events.click(props.name));
+  let editBtn = create("button", { class: "btn" });
+  editBtn.innerText = "Edit";
+  deleteBtn.addEventListener("click", function () {
+    li.remove();
+    deleteContact(props.contact);
+    console.log(getContact());
+  });
+  editBtn.addEventListener("click", function () {
+    console.log("Hello");
+  });
+
+  controller.append(deleteBtn, editBtn);
+  li.append(avatar, divGroup, controller);
 
   return li;
 }
-
 // let props = {
 //   name: "Mushfiqur Niloy",
 //   phone: "01788689889",
